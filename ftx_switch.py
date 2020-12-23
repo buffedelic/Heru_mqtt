98385262713d2dab8d52aaeb762612b1178ecd65
Christofer Andersson <buffedelic@hotmail.com>
2020-12-24 00:29:39 +0100

    Working with mqtt, TODO Real world

--- ftx_switch.py
+++ ftx_switch.py
@@ -4,6 +4,10 @@
 from heru import HeruFTX
 import minimalmodbus
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import time
+
+
 
 instr = HeruFTX('/dev/ttyUSB0', 1) #port, slaveadress
 instr.mode = minimalmodbus.MODE_RTU
@@ -11,13 +15,35 @@ instr.serial.timeout  = 0.2 #Timeout might be adjusted to allow full reading of 
 instr.debug = False
 instr.precalculate_read_size = False
 
+topics = ["hvac/heru/power/set",
+         "hvac/heru/over_pressure_mode/set",
+         "hvac/heru/boost_mode/set",
+         "hvac/heru/away_mode/set",
+         "hvac/heru/clear_alarms/set",
+         "hvac/heru/clear_filter_alarms/set"] 
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+    #function to anounce availability
+    print("Subscribed: "+str(mid)+" "+str(granted_qos)+" "+str(userdata)+" "+str(client))
+    print("Polling register " + str(mid) + " " + topics[ int(mid) - 1 ])
+    poll_device(int(mid))
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload)) 
+    #handle message
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    print("Setting register " + str(topics.index(msg.topic) + 1) + " " + msg.topic)
+    instr.set_coil_status(topics.index(msg.topic) + 1, int(msg.payload))
 
+def poll_device(register):
+    answer = None
+    while answer is None:
+        try:
+            answer = instr.get_coil_status(register)
+        except:
+            pass         
+    publish.single(topics[register - 1], answer, qos = 2, retain=True, hostname="homeassistant.lan", auth={'username':"buff", 'password':"mammas"}, client_id="IVT")
+
+    
 
 if __name__ == '__main__':
 
@@ -29,21 +55,30 @@ if __name__ == '__main__':
 #0x00005 Clear Alarms
 #0x00006 Reset filter timer
 ###########################
+    
+    broker_adress='homeassistant'
+    subscribe_topic='hvac/heru/power/set'
+    print("Connecting to broker {}".format(broker_adress))
+    client = mqtt.Client("Heru asdasdasdaasdas")
+    client.username_pw_set("buff", "mammas")
+    client.connect(broker_adress, 1883)
+    client.clean_session = True
+    client.on_subscribe = on_subscribe
+    #client.on_connect = on_connect
+    client.on_message = on_message
 
-	instr.set_coil_status(3, "on")
+    for topic in topics:
+        client.subscribe(topic)
+    # client.loop_start()
+    client.loop_forever()
 
-	broker_adress='homeassistant.lan'
-	subscribe_topic='hvac/heru/power/set'
+    # while True:
+        # pass
+        # for i in range(1, 7):
+        #     print("polling register: " + str(i))
+        #     poll_device(i)
+        #     time.sleep(2)
+        # time.sleep(15)
 
-	print("Connecting to broker {}".format(broker_adress))
-	client = mqtt.Client("Heru Client")
-#	client.username_pw_set("buff", "mammas")
-	client.on_subscribe = on_subscribe
-#	client.on_connect = on_connect
-	client.connect(broker_adress, 1883)
-	print(client.subscribe("hvac/heru/mode/set"))
-	client.loop_forever()
-#	print(client.subscribe(subscribe_topic))
-#	client.publish("hvac/heru/power/set","on")
 
 pass
