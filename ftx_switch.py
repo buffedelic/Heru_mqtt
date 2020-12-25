5491021bfcfa3fd4e259563eacc65173741dcd57
Christofer Andersson <buffedelic@hotmail.com>
2020-12-26 00:12:53 +0100

    stable polling

--- ftx_switch.py
+++ ftx_switch.py
@@ -6,6 +6,7 @@ import minimalmodbus
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import time
+import schedule
 
 
 
@@ -22,28 +23,43 @@ switch = ["hvac/heru/power/set",
          "hvac/heru/clear_alarms/set",
          "hvac/heru/clear_filter_alarms/set"] 
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    for topic in switch:
+        client.subscribe(topic)
+
 def on_subscribe(client, userdata, mid, granted_qos):
     #function to anounce availability
     print("Subscribed: "+str(mid)+" "+str(granted_qos)+" "+str(userdata)+" "+str(client))
-    print("Polling register " + str(mid) + " " + switch[ int(mid) - 1 ])
+    #print("Polling register " + str(mid) + " " + switch[ int(mid) - 1 ])
     poll_device(int(mid))
 
 def on_message(client, userdata, msg):
     #handle message
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
-    print("Setting register " + str(switch.index(msg.topic) + 1) + " " + msg.topic)
+    #print("Setting register " + str(switch.index(msg.topic) + 1) + " " + msg.topic)
     instr.set_coil_status(switch.index(msg.topic) + 1, int(msg.payload))
 
 def poll_device(register):
-    answer = None
-    while answer is None:
-        try:
-            answer = instr.get_coil_status(register)
-        except:
-            pass         
-    publish.single(switch[register - 1], answer, qos = 2, retain=True, hostname="homeassistant.lan", auth={'username':"buff", 'password':"mammas"}, client_id="IVT")
+    try:
+        answer = instr.get_coil_status(register)
+    except:
+        pass 
+    publish.single(switch[register - 1], answer, qos = 2, retain=True, hostname="homeassistant.lan", auth={'username':"buff", 'password':"mammas"}, client_id="Heru Control")
 
-    
+def update_functions():
+    for i in range(1, 5):
+        poll_device(i)
+        time.sleep(2)
+
+def update_alarms():
+    for i in range(5, 7):
+        poll_device(i)
+        time.sleep(2)
+
 
 if __name__ == '__main__':
 
@@ -63,21 +79,21 @@ if __name__ == '__main__':
     client.username_pw_set("buff", "mammas")
     client.connect(broker_adress, 1883)
     client.on_subscribe = on_subscribe
-    #client.on_connect = on_connect
     client.on_message = on_message
+    client.on_connect = on_connect
 
-    for topic in switch:
-        client.subscribe(topic)
+
     client.loop_start()
     # client.loop_forever()
 
+    # while True:
+    #     pass
+        # time.sleep(15)
+    schedule.every(5).minutes.do(update_functions)
+    schedule.every(1440).minutes.do(update_alarms)
     while True:
-        # pass
-        time.sleep(15)
-        for i in range(1, 7):
-            print("polling register: " + str(i))
-            poll_device(i)
-            time.sleep(2)
+        schedule.run_pending()
+        time.sleep(1)
 
 
 pass
